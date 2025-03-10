4b7b4ca0070494e984dd9dfafd9b3b3eca7ff7c9
Balajibcasiga <144203535+Balajibcasiga@users.noreply.github.com>
2025-03-10 15:54:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -34,9 +34,7 @@ function fetchWeather Data(city)
 
 {
 
-const apiKey =
-
-YOUR_OPENWEATHER_API_KEY';
+const apiKey =1bb1ce6e958246e398693705251003';
 
 ' const url = `https://api .openweathermap.org/data/2 .5/weather?q=${city}&appid= ${apiKey}&units=metric`;
 
